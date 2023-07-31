b9dd0fd8760ae9d53d1a7afcb60637759632fd77
KevsterPerssion <107465465+KevsterPersson@users.noreply.github.com>
2023-07-31 13:22:35 +0800

    Add files via upload

--- main.py
+++ main.py
@@ -1,50 +1,58 @@
 import gc
 
 from function import function_3DMark as func_3DMark, function_2077 as func_2077, function_Bilibili as func_Bilibili, \
-    function_FurMark as func_FurMark, function_iQIYI as func_iQIYI, function_TombRaider as func_Raider
+    function_FurMark as func_FurMark, function_iQIYI as func_iQIYI, function_TombRaider as func_Raider, \
+    function_TencentVideo as func_TencentVideo
 import time
 import global_var as gv
 
 
 # 赛博朋克2077
 def run_2077():
-    gv.count_max = 5  # 设置外循环次数
-    gv.sub_count_max = 10  # 设置内循环次数
+    gv.count_max_2077 = 5  # 设置外循环次数
+    gv.sub_count_max_2077 = 10  # 设置内循环次数
     func_2077.func_cyberpunk()
 
 
-# 哔哩哔哩网页端视频播放
+# 哔哩哔哩
 def run_Bilibili():
-    gv.video_sec = 300  # 设置播放时长
-    gv.count_max = 200  # 设置循环次数
+    gv.video_sec_bilibili = 300  # 设置播放时长
+    gv.count_max_bilibili = 200  # 设置循环次数
     func_Bilibili.func_bilibili()
 
 
-# 3DMark测试
+# 3DMark
 def run_3DMark():
-    gv.count_max = 30  # 设置循环次数
+    gv.count_max_3DMark = 30  # 设置循环次数
     func_3DMark.func_3DMark()
 
 
-# 爱奇艺客户端播放
+# 爱奇艺
 def run_iQIYI():
-    gv.sleep_time = 1000  # 设置播放时长设置
-    gv.max_count = 20  # 设置循环次数设置
+    gv.sleep_time_iQIYI = 1000  # 设置播放时长设置
+    gv.max_count_iQIYI = 20  # 设置循环次数设置
     func_iQIYI.func_iQIYI()
 
 
 # 古墓丽影暗影
 def run_TombRaider():
-    gv.count_max = 50  # 设置外循环次数
-    gv.sub_count_max = 5  # 设置内循环次数
+    gv.count_max_TombRaider = 50  # 设置外循环次数
+    gv.sub_count_max_TombRaider = 5  # 设置内循环次数
     func_Raider.func_TombRaider()
 
 
-# FurMark测试
+# FurMark
 def run_FurMark():
-    gv.count_max = 200  # 设置循环次数
-    gv.sleep_time = 300  # 设置每次运行时长
+    gv.count_max_FurMark = 200  # 设置循环次数
+    gv.sleep_time_FurMark = 300  # 设置每次运行时长
     func_FurMark.func_FurMark()
+
+
+# 腾讯视频
+def run_TencentVideo():
+    gv.tencent_sleep_time = 300  # 默认播放时长设置
+    gv.tencent_max_count = 5  # 默认循环次数设置
+    func_TencentVideo.func_TencentVideo()
 
 
 if __name__ == "__main__":
@@ -53,8 +61,7 @@ if __name__ == "__main__":
     # 计时器
     section_start = time.time()
 
-    # 执行区
-    run_Bilibili()
+    # run
 
     # 计时器
     section_finish = time.time()
