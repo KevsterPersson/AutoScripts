e0b755cf43cbb87a15f24950be48b0d4d7769a98
KevsterPerssion <107465465+KevsterPersson@users.noreply.github.com>
2023-07-31 13:21:52 +0800

    Add files via upload

--- global_var.py
+++ global_var.py
@@ -1,6 +1,33 @@
-global count_times
-global count_max
-global sub_count_max
-global sleep_time
-global max_count
-global video_sec
+# 腾讯视频
+global tencent_sleep_time
+global tencent_max_count
+global tencent_count_times
+
+# 3DMark
+global count_times_3DMark
+global count_max_3DMark
+
+# 2077
+global count_times_2077
+global count_max_2077
+global sub_count_max_2077
+
+# Bilibili
+global count_times_bilibili
+global count_max_bilibili
+global video_sec_bilibili
+
+# FurMark
+global count_max_FurMark
+global count_times_FurMark
+global sleep_time_FurMark
+
+# iQIYI
+global sleep_time_iQIYI
+global max_count_iQIYI
+global count_times_iQIYI
+
+# TombRaider
+global count_times_TombRaider
+global count_max_TombRaider
+global sub_count_max_TombRaider
