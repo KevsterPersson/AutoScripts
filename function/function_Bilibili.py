6887f65252e9d40f1a1fbd78811139c05e424b1c
KevsterPerssion <107465465+KevsterPersson@users.noreply.github.com>
2023-09-11 16:21:20 +0800

    Add files via upload

--- function/function_Bilibili.py
+++ function/function_Bilibili.py
@@ -20,35 +20,19 @@ def func_bilibili():
 
     # 测试项
     gv.count_times_bilibili = 1
-    # count = gv.count_times_bilibili
     while gv.count_times_bilibili < gv.count_max_bilibili:
         print('--- 第', gv.count_times_bilibili, '次 ---', '\n')
         round_start = time.time()
-        time.sleep(3)
-        bilibili.find_icon_and_click('Pictures/EdgeBilibili/win_button_search.png')
-        bilibili.keyboard_write('Microsoft Edge', 0.2, 1)
-        bilibili.keyboard_press('enter', 3)
-        bilibili.check_button_state('Pictures/EdgeBilibili/edge_button_min_window.png',
-                                    'Pictures/EdgeBilibili/edge_button_max_window_state.png')
-        bilibili.find_icon_and_click('Pictures/EdgeBilibili/edge_search_button.png')
-        bilibili.keyboard_write('bilibili.com', 0.2, 1)
-        bilibili.keyboard_press('enter', 4)
-        bilibili.find_icon_and_click('Pictures/EdgeBilibili/edge_bilibili_button_dianshiju.png')
-        bilibili.mouse_move_and_click(1241, 682, 1494, 682, 1746, 682)
-        bilibili.get_position_and_move_to('Pictures/EdgeBilibili/edge_bilibili_login_button.png')
-        bilibili.get_position_and_move_to('Pictures/EdgeBilibili/edge_bilibili_button_sort.png')
-        pya.moveTo(2251, 350, 1)
-        pya.scroll(600)
-        bilibili.check_button_state('Pictures/EdgeBilibili/edge_bilibili_button_1_unclick.png',
-                                    'Pictures/EdgeBilibili/edge_bilibili_button_1_clicked.png')
-        time.sleep(5)
+        bilibili.webbrowser('https://www.bilibili.com/bangumi/play/ep703938?from_spmid=666.25.episode.0',
+                            'https://www.bilibili.com/bangumi/play/ep761273?from_spmid=666.25.episode.0',
+                            'https://www.bilibili.com/bangumi/play/ep764993?from_spmid=666.25.episode.0', 5)
         bilibili.move_to_and_double_click(1560, 1014)
         print('第', gv.count_times_bilibili % 3, '段视频播放', gv.video_sec_bilibili, '秒')
         time.sleep(gv.video_sec_bilibili)
         bilibili.move_to_and_double_click(1724, 1096)
-        bilibili.find_icon_and_click('Pictures/EdgeBilibili/edge_button_max_window_state.png')
         bilibili.find_icon_and_click('Pictures/EdgeBilibili/edge_button_close.png')
 
+        # 计时器
         round_finish = time.time()
         round_time = round_finish - round_start
         print('测试耗时', round(round_time), '秒完成', '\n')
